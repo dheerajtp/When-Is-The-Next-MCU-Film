9812dab1ecc516918ea30304b4a490f7d9a8b4ee
Dheeraj TP <45311336+dheerajtp@users.noreply.github.com>
2022-03-23 21:32:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen min-w-screen justify-center items-center">
       <img src={logo} alt="marvel-logo" className="mt-1 w-1/3"/>
+      <h1>When Is The Next MCU Film?</h1>
       {isLoading && <Loading/>}
       {!isLoading && !isError && <Movie days_until={days_until} overview={overview} poster_url={poster_url} release_date={release_date} title={title} type={type} nextoverview={nextoverview} nextimage={nextposter}  nexttitle={nexttitle} nextdate={nextdate} following_production={following_production}/>}
       {!isLoading && isError && <Empty/>}
